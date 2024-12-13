33f265eb07585a3bf5176dedd3522cfeddcb6faf
thadar <thadarshinhtet2019@gmail.com>
2024-12-13 14:41:38 +0630

    adding Avatar

---
+++ app/components/Avatar.jsx
@@ -0,0 +1,37 @@
+'use client'
+import { useRive, useStateMachineInput } from "@rive-app/react-canvas";
+import React, { useEffect } from 'react'
+
+const Avatar = () => {
+
+    const {rive, RiveComponent} = useRive({
+        src: "/assets/allcharacter.riv",
+        artboard: "AvatarArtboard",
+        stateMachines: "State Machine 1",
+        autoplay: true,
+       
+      });
+
+
+      const isTalking=useStateMachineInput(rive,'State Machine 1', 'isTalking')
+      const isEyeMove=useStateMachineInput(rive,'State Machine 1', 'isEyeMove')
+      const isDisgust=useStateMachineInput(rive,'State Machine 1', 'isDisgust')
+
+
+
+      useEffect(()=>{
+        if(rive && isTalking && isEyeMove && isDisgust){
+            isTalking.value=true
+            isEyeMove.value=true
+            isDisgust.value=true
+        }
+      },[rive,isTalking])
+  return (
+    <div className="w-[500px] h-[100vh]">    
+ 
+     <RiveComponent style={{ width: '100%', height: '100%' }}  />
+</div>
+  )
+}
+
+export default Avatar
--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,14 @@ import LogOutModal from '@/app/components/LogOutModal'
 import StudentEdit from '@/app/components/StudentEdit'
 import TeacherEdit from '@/app/components/TeacherEdit'
 import RobotChat from '@/app/components/RobotChat'
+import Avatar from '@/app/components/Avatar'
+
 
 
 
 
 export default function Home() {
   return (
-  <RobotChat/>
+  <Avatar/>
   );
 }
